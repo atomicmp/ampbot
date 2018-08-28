de83e1c0022cb5a8d17d2dd1d083695f6ec1f0e9
Miles <miles.b.flo@gmail.com>
2018-08-27 23:50:31 -0700

    Set up basic production migration flow

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ const {
   POSTGRES_USER,
   POSTGRES_PASSWORD,
   POSTGRES_DATABASE,
+  PROD_POSTGRES_CONNECTION
 } = process.env;
 
 module.exports = {
@@ -46,17 +47,17 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: POSTGRES_DATABASE,
-      user: POSTGRES_USER,
-      password: POSTGRES_PASSWORD
-    },
+    connection: PROD_POSTGRES_CONNECTION,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: POSTGRES_DATABASE
+      tableName: POSTGRES_DATABASE,
+      directory: './src/services/database/migrations'
+    },
+    seeds: {
+      directory: './src/services/database/seeds'
     }
   }
 }
