cc8fb513497c7352e8fa3f7cd510dbf2ee1fefdc
Miles <14067660+milesflo@users.noreply.github.com>
2018-08-28 19:51:38 -0700

    Adding beta version of roleGrant command

---
+++ src/commands/grant-role-key.js
@@ -0,0 +1,45 @@
+const isEmpty = require('lodash.isempty');
+const first = require('lodash.first');
+const { WEBSITE_URL } = require('../utils/constants');
+const db = require('../services/database');
+
+const { insertKey } = require('../helpers');
+
+const texts = {
+  NO_TARGETS: () => 'This command requires targets. Please mention a role.',
+  NOT_ADMIN: () => 'This command can only be used by ADMINISTRATOR ranks.',
+  HAS_KEY: target => `${target.displayName} already has a key!`,
+  KEY_MESSAGE: key =>
+    `Here's your key! \`${key}\`\n\nYou can redeem it at ${WEBSITE_URL}/register.\n\n_See you in the Wasteland!_`,
+};
+
+module.exports = async function acceptApplicationCommand(msg) {
+  const { author, member } = msg;
+  if (!member.permissions.has('ADMINISTRATOR')) {
+    await msg.channel.send(texts.NOT_ADMIN());
+    return;
+  }
+
+  const targetRole = first(msg.mentions.roles.array())
+  const targets = targetRole.members.array();
+
+  if (isEmpty(targets)) {
+    await msg.channel.send(texts.NO_TARGETS());
+    return;
+  }
+  await Promise.all(
+    targets.map(async target => {
+      const targetHasKey = !isEmpty(
+        await db('keys').where('discord_id', target.id)
+      );
+      if (targetHasKey) {
+        // await msg.channel.send(texts.HAS_KEY(target));
+      } else {
+        const key = await insertKey({ author, target });
+
+        await target.send(texts.KEY_MESSAGE(key));
+      }
+    })
+  );
+  msg.channel.send(`${targetRole.name} complete!`)
+};
--- src/commands/index.js
+++ src/commands/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   ping: require('./ping'),
   acceptApplication: require('./accept-application'),
+  grantRoleKey: require('./grant-role-key'),
 };
