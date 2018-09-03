28993407393c897eb9d4e19fc5ffa148cd42a1e6
Miles <miles.b.flo@gmail.com>
2018-09-02 23:40:36 -0700

    Sending purged users a status message

--- src/commands/cleanup-testers.js
+++ src/commands/cleanup-testers.js
@@ -19,6 +19,7 @@ module.exports = async function cleanupTestersCommand(msg) {
       if (!forumUsers.has(targetId)) {
         console.log(target.user.username, "does not have a forum account")
         await db('keys').where('discord_id', targetId).del()
+        await target.send('You have been removed from Testers due to inactivity. Your key has been invalidated. Please reapply through the normal channels.')
         await target.removeRole(TestersRole)
       } else {
 
