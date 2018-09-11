b06397b06372e718d3ac64ed3c1d4753c89c0e28
Miles <miles.b.flo@gmail.com>
2018-09-11 01:41:26 -0700

    Created way to initialize faction in Discord, because to forget seed data amirite

---
+++ src/commands/faction-init.js
@@ -0,0 +1,43 @@
+const isEmpty = require('lodash.isempty')
+const db = require('../services/database');
+
+const texts = {
+  NOT_ADMIN: () => 'This command can only be used by ADMINISTRATOR ranks.',
+}
+module.exports = async function factionInitCommand(msg) {
+  const {
+    member
+  } = msg;
+  const acknowledgeEmoji = msg.guild.emojis.find(emoji => emoji.name === 'puppetdab');
+
+  if (!member.permissions.has('ADMINISTRATOR')) {
+    await msg.channel.send(texts.NOT_ADMIN());
+    return;
+  }
+  const [targetRole] = msg.mentions.roles.array();
+  if (!targetRole) {
+    await msg.channel.send('This command requires a target role.');
+    return;
+  }
+  const factionExists = !isEmpty(
+    await db('factions').where('role_id', targetRole.id)
+  );
+
+  if (factionExists) {
+    await msg.channel.send(`${targetRole.name} faction already initialized!`);
+    return;
+  }
+  const [factionId] = await db('factions').insert({
+    faction_name: targetRole.name,
+    color: targetRole.hexColor,
+    role_id: targetRole.id
+  }).returning('faction_id')
+  console.log(factionId)
+  const factionMembers = targetRole.members.array();
+
+  await Promise.all(
+    factionMembers.map(member =>
+      db('users').where('discord_id', member.user.id).update('faction', factionId))
+  )
+  await msg.react(acknowledgeEmoji);
+}
--- src/commands/index.js
+++ src/commands/index.js
@@ -4,5 +4,6 @@ module.exports = {
   grantRoleKey: require('./grant-role-key'),
   dab: require('./dab'),
   cleanupTesters: require('./cleanup-testers'),
-  bruh: require('./bruh')
+  bruh: require('./bruh'),
+  factionInit: require('./faction-init')
 };
