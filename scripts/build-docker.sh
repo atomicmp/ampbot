8a7b50730c7cbf98ee124d44ee1d9f4e711897b7
Miles <miles.b.flo@gmail.com>
2019-10-17 23:17:18 -0700

    Dockerizing application for production deployment

---
+++ scripts/build-docker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+location=$( dirname "$(realpath $0)" )
+
+source "$location/../.env"
+
+docker create \
+    -e WEBSITE_URL=$WEBSITE_URL \
+    -e COMMAND_PREFIX=$COMMAND_PREFIX \
+    -e DISCORD_TOKEN=$DISCORD_TOKEN \
+    -e POSTGRES_HOST=$POSTGRES_HOST \
+    -e POSTGRES_USER=$POSTGRES_USER \
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -e POSTGRES_DB=$POSTGRES_DB \
+    --name 'ampbot' \
+    amp/bot
--- src/services/discord.ts
+++ src/services/discord.ts
@@ -1,6 +1,10 @@
 import Discord from "discord.js";
 const { DISCORD_TOKEN } = process.env;
+
+if (!DISCORD_TOKEN) throw Error("Token not found!");
+
 const client = new Discord.Client();
+
 client.login(DISCORD_TOKEN);
 
 export default client;
